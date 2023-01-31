eaa83531c3250e7c974b27f5d81dd4cd7c69bec5
Diba00 <dibas.shishegar@gmail.com>
2023-01-31 13:02:19 +0100

    min første commit

---
+++ oblig1.py
@@ -0,0 +1,9 @@
+num = [1,2 [3,4],5]
+
+print ("first element", num[0])
+print ("last element", num[3])
+print ("nested list", num[2])
+print ("sub item of item 3", num[2][0])
+
+for i in num:
+    print (i)
